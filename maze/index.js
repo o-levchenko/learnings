9c80dbd58eba8fc2b5b7104b1c7e4e95d0f9255a
Olha Levchenko <levchenko.olya@gmail.com>
2022-05-21 22:31:51 +0200

    Finish up the maze game

--- maze/index.js
+++ maze/index.js
@@ -1,9 +1,16 @@
-const { Engine, Render, Runner, Composite, World, Bodies } = Matter;
+const { Engine, Render, Runner, Composite, World, Bodies, Body, Events } = Matter;
 
 const width = 600;
 const height = 600;
 
+const rows = 6;
+const columns = rows * width / height;
+
+const cellWidth = width / columns;
+const cellHeight = height / rows
+
 const engine = Engine.create();
+engine.world.gravity.y = 0;
 const { world } = engine;
 const render = Render.create({
     element: document.body,
@@ -18,14 +25,12 @@ Render.run(render);
 Runner.run(Runner.create(), engine);
 
 // Walls
-const rows = 5;
-const columns = 3;
 
 const walls = [
-    Bodies.rectangle(width/2, 0, width, 40, { isStatic: true }),
-    Bodies.rectangle(width / 2, height, width, 40, { isStatic: true }),
-    Bodies.rectangle(0, height / 2, 40, height, { isStatic: true }),
-    Bodies.rectangle(width, height / 2, 40, height, { isStatic: true })
+    Bodies.rectangle(width / 2, 0, width, 10, { isStatic: true }),
+    Bodies.rectangle(width / 2, height, width, 10, { isStatic: true }),
+    Bodies.rectangle(0, height / 2, 10, height, { isStatic: true }),
+    Bodies.rectangle(width, height / 2, 10, height, { isStatic: true })
 ];
 Composite.add(world, walls);
 
@@ -111,6 +116,113 @@ const stepThroughCell = (row, column) => {
 };
 
 stepThroughCell(startRow, startColumn);
-console.log(startRow, startColumn);
-console.log(verticals);
-console.log(horizontals);
+
+horizontals.forEach((row, rowIndex) => {
+    row.forEach((open, columnIndex) => {
+        if (open) {
+            return;
+        }
+        const wall = Bodies.rectangle(
+            columnIndex * cellWidth + cellWidth / 2 ,  
+            rowIndex * cellHeight + cellHeight,
+            cellWidth,
+            10,
+            {
+                label: 'wall',
+                isStatic: true
+            }
+        );
+        Composite.add(world, wall);
+    })
+});
+
+verticals.forEach((row, rowIndex) => {
+    row.forEach((open, columnIndex) => {
+        if (open) {
+            return;
+        }
+        const wall = Bodies.rectangle(
+            columnIndex * cellWidth + cellWidth,
+            rowIndex * cellHeight + cellHeight / 2,
+            10,
+            cellHeight,
+            {
+                label: 'wall',
+                isStatic: true
+            }
+        );
+        Composite.add(world, wall);
+    });
+});
+
+// Goal
+
+const goal = Bodies.rectangle(
+    width - cellWidth / 2,
+    height - cellHeight / 2,
+    cellWidth * .7,
+    cellHeight * .7,
+    {
+        label: 'goal',
+        isStatic: true
+    }
+);
+Composite.add(world, goal);
+
+// Ball
+
+const ball = Bodies.circle(
+    cellWidth / 2,
+    cellHeight / 2,
+    cellWidth / 4, 
+    {
+        label: 'goal'
+    }
+);
+Composite.add(world, ball)
+
+
+// Move ball controls
+
+document.addEventListener('keydown', event => {
+    const { x, y } = ball.velocity;
+    console.log(x, y);
+
+    if (event.keyCode === 87) {
+        console.log('move ball up');
+        Body.setVelocity(ball, {x, y: y - 5});
+    }
+
+    if (event.keyCode === 68) {
+        console.log('move ball right');
+        Body.setVelocity(ball, {x: x + 5, y});
+    }
+
+    if (event.keyCode === 83) {
+        Body.setVelocity(ball, {x, y: y + 5});
+    }
+
+    if (event.keyCode === 65) {
+        Body.setVelocity(ball, {x: x - 5, y});
+    }
+});
+
+// Wind Condition
+
+Events.on(engine, 'collisionStart', event => {
+    event.pairs.forEach(collision => {
+        const labels = ['ball', 'goal'];
+
+        if (
+            labels.includes(collision.bodyA.label) &&
+            labels.includes(collision.bodyB.label)
+        ) {
+            world.gravity.y = 1;
+            world.bodies.forEach(body => {
+                if (body.label === 'wall') {
+                    Body.setStatic(body, false);
+                }
+            })
+        }
+    });
+});
