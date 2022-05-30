de74e56aedaa0ca880b149036679e790a4f468ca
OlhaLevchenko <o.levchenko@eyeo.com>
2022-05-30 10:03:27 +0200

    Added list functionality

---
+++ list/index.js
@@ -0,0 +1,34 @@
+#!/usr/bin/end node
+
+const fs = require('fs');
+const util = require('util');
+const chalk = require('chalk');
+const path = require('path');
+
+const { lstat } = fs.promises
+
+// console.log(argv);
+
+const targetDir = process.argv[2] || process.cwd();
+
+fs.readdir(targetDir, async (err, filenames) => {
+    if (err) {
+        console.log(err);
+    }
+
+    const statPromises = filenames.map(filename => {
+        return lstat(path.join(targetDir, filename));
+    })
+
+    const allStats = await Promise.all(statPromises);
+
+    for (let stats of allStats) {
+        const index = allStats.indexOf(stats);
+
+        if (stats.isFile()) {
+            console.log(filenames[index]);
+        } else {
+            console.log(chalk.bold(filenames[index]));
+        }   
+    } 
+});
